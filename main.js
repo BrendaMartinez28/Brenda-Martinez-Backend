bb07f8c565a4c6ef91205b295b68b58225cda7dd
BrendaMartinez28 <125223171+BrendaMartinez28@users.noreply.github.com>
2023-07-25 23:22:24 -0300

    preentrega

---
+++ main.js
@@ -0,0 +1,28 @@
+const ProductManager = [];
+
+class Product {
+    constructor(title, description, price, thumbnail, code, stock) {
+        this.title = title
+        this.description = description
+        this.price = price
+        this.thumbnail = thumbnail
+        this.code = code
+        this.stock = stock
+    }
+
+    addProduct() {
+        console.log("agregue un producto")
+    }
+
+    getPrducts() {
+        console.log("Tome un producto");
+    }
+
+    getProductsById() {
+        console.log("id de product");
+    }
+}
+
+let nombre = "Brenda";
+
+console.log(`Hola ${nombre}`);
